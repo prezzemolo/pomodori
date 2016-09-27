f7771cb511fe3f7a4b750db7265303ccd5326df8
prezzemolo <git@prezzemolo.ga>
2016-09-28 06:16:16 +0900

    pomodori v0.04

--- pomodori/api.php
+++ pomodori/api.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/nicovideo.php';
 
 class api
 {
-    const version = 'v0.03';
+    const version = 'v0.04';
 
     private function default_header() {
         header('Content-Type: application/json');
