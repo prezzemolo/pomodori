c1f0b70543cd90d4293d105396c084982d3eec76
prezzemolo <git@prezzemolo.ga>
2016-10-08 21:49:17 +0900

    rollback

--- index.php
+++ index.php
@@ -19,22 +19,4 @@ if (!isset($param))
     $api->$route();
 else
     $api->$route($param);
-?><?php
-require_once __DIR__.'/pomodori/router.php';
-require_once __DIR__.'/pomodori/api.php';
-
-$router = new pomodori\router();
-$router->set_param(array(
-    'url' => isset($_SERVER['QUERY_STRING'])
-        ? str_replace('?'.$_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI'])
-        : $_SERVER['REQUEST_URI'],
-    'method' => $_SERVER['REQUEST_METHOD'],
-    'param' => $_REQUEST));
-list($route, $param) = $router->parse();
-
-$api = new pomodori\api();
-if (!isset($param))
-    $api->$route();
-else
-    $api->$route($param);
 ?>
